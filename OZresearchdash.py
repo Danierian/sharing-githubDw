f4671c7c273b56bdffe92dc9eebe77419e7bec0f
Danierian <nariian11@gmail.com>
2020-12-15 17:51:18 -0500

    Update OZresearchdash.py

--- OZresearchdash.py
+++ OZresearchdash.py
@@ -148,7 +148,7 @@ data1 = [trace2, trace3, trace1]
 
 layout = {
 
-    "title": "Unemployment",
+    "title": "Unemployment Rate by State",
     "xaxis": {
         "ticks": "",
         #  "autotick": True,
@@ -243,7 +243,7 @@ data4 = [trace4, trace6, trace5]
 
 layout = {
 
-    "title": "Labor Force Participation",
+    "title": "Labor Force Participation Rate by State",
     "xaxis": {
         "ticks": "",
         #  "autotick": True,
@@ -301,29 +301,31 @@ app.layout = html.Div([
     html.Div(id='output_container', children=[]),
     html.Br(),
     dcc.Markdown('''
-  *The data used in this preliminary analysis comes from the 2016 American Community Survey (ACS), five year estimates*  
+  *The data used in this preliminary analysis comes from the 2016 American Community Survey (ACS), five year estimates*.  
 #### Opportunity Zone Selection Process
-To be eligible for designation, a census tract must 
+To be eligible for designation, a census tract must :
 * Have a poverty rate of at least 20 percent; or
 * Have a median income below 80 percent of the State or metropolitan area
 * Be contiguous with a census tract meeting one of the above conditions and have median income less than 125% of the 
 qualifying contiguous census tract. 
 
-According to the analysis, there are 8,540 low-income tracts and 245 contiguous income tracts. These numbers are 
+According to this analysis, there are 8,540 low-income tracts and 245 contiguous income tracts. These numbers are 
 compared with the Council of Economic Advisors (CEA) report that has 8,537 low-income tracts and 229 contiguous. 
 The difference in numbers come from census tracts that may have been split and has the same census tract number 
-(*i.e pennisula tracts). 
+(* *i.e pennisula tracts*). When analysis on a per state level, the number of Opportunity Zones (OZs) per state matches the CEA report
+for all inland states but there are several additional tracts listed as OZs in Guam that have the same census tract number.
 
 The data provided also has geographical capabilities enabling the mapping of eligible census tracts that meet the 
 qualifications above as well as the census tracts that are currently designated as Opportunity Zones.
-*graph is available* 
+
+*Graph is available* 
 
 #### Demographics
 Opportunity Zones were created in an attempt to lower capital taxes but with a focus on distressed communities. Below
-is a presentation of the differences in designated and non-designated census tracts. The analysis finds that the average 
-poverty rate for census tracts designated as OZs are almost double that of all other census tracts and have higher shares
+is a presentation of the differences in designated and non-designated OZ census tracts. This analysis finds that the average 
+poverty rate for census tracts designated as OZs are a little below double that of all other census tracts and have higher shares
 of Hispanics, African Americans, and Adults without Diploma. This is good to see as the purpose behind OZs is to serve areas
-that are the more impoverished communities in the United States. 
+that are considered to be more impoverished communities compared to others in the United States. 
 
 '''),
     dcc.Graph(
@@ -334,15 +336,16 @@ html.Br(),
     dcc.Markdown('''
 #### Poverty Rates
 
-The CEA states that, *"Governors could designate up to 25 percent of their qualifying census tracts, or up to 25 tracts
+The CEA states that, **"Governors could designate up to 25 percent of their qualifying census tracts, or up to 25 tracts
 for those States with fewer than 100 eligible tracts. Eligible, contiguous tracts were restricted
-to make up no more than 5 percent of designated OZs in any State. "*
+to make up no more than 5 percent of designated OZs in any State. "**
 
 The above figure shows that on average, OZs have higher poverty rates than non-designated tracts but it is important
-to dive a little deeper to see if this holds true on a state level. Below is a figure that compares the 
+to conduct futher analysis to determine if this holds true on a state level. Below is a figure that compares the 
 average poverty rate of designated OZs, eligible OZs (not designated), and non-eligible census tracts on a state level.
 The figure below shows that for every state, there is a higher average poverty rate for the designated OZs compared to
-eligible OZs (not designated), and non-eligible census tracts.
+eligible OZs (not designated), and non-eligible census tracts for each state. This can be used to verify that so far the designation
+process is somewhat efficient as the designated OZs on average align with the purpose of the program.
 
 *_Note: The poverty rate estimates are from 2016 as these are the estimations that were used for designation._
 
@@ -356,8 +359,11 @@ html.Br(),
     dcc.Markdown('''
 #### Labor Metrics by State
 
-Below are some additional economic metrics related to unemployment. Each graph compares the unemployment rate
-and labor force participation rate of designated OZs, eligible but not Designated and non eligible census tracts.
+Below are some additional economic metrics related to unemployment. Each graph compares the Unemployment Rates
+and Labor Force Participation Rates of designated OZs, eligible OZs (not designated) and the United States (as a whole).
+Figure 4 and Figure 5 both shows that on average, designated OZs have a higher unemployment rates and lower 
+labor force particpation compared to the United States' average. Hopefully with OZs being designated in such areas, 
+this will lead to job creation and give these areas a much needed economic boost that has been long overdue. 
 
 '''),
     dcc.Graph(
@@ -374,16 +380,19 @@ html.Br(),
 
 After conducting this preliminary analysis, the hope is to use this information as the building blocks for deeper 
 analysis. After speaking with Randall Goodnight and Leon Vertz, one additional source of data is CBRE analytics 
-(*comparable to dodge data*). This particular data set goes back to 2000 and has information on construction and 
-development projects in areas. Each project has lat/long, zip code, county, and state information. This dataset has 
-information which includes start date, stage of planning, as well as contact information on owner, contractors and 
-architects. 
+(*comparable to FW-Dodge data*). This particular data set goes back to 2000 and has information on construction and 
+development projects in areas. Each project has `lat/long`, `zip code`, `county`, and `state` information. This dataset has 
+information which includes start date, stage of planning, as well as contact information on `owner`, `contractors` and 
+`architects`. 
 
 Given the possible geo-capabilites, it may be possible to overlay markers representing construction projects that 
 have been started since 2015 on top of the graph that we already have displaying eligible and designated census tracts.
 It may take some time given it is raw data that stills needs to be clean and is currently not presented on the census 
 tract level. However, we do have exact lat/long coordinates and it is rumored that HUD users has a crosswalk between 
 zip codes and census tracts. 
+
+I have not been able to access this data yet but I will be getting a small snippet of it (3 states) to see what I can do with it
+but judging from the conservation it seems promising. 
 
 '''),
     html.Br(),
