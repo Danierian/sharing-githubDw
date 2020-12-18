5cc1ea53390612e839bd3f4db4911a09503cc29f
Danierian <nariian11@gmail.com>
2020-12-18 17:18:40 -0500

    Update OZresearchdash.py

--- OZresearchdash.py
+++ OZresearchdash.py
@@ -10,7 +10,7 @@ import dash_table
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server=app.server
 # ------------------------------------------------------------------------------------------
 #
 # Importing Data
